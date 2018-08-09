522d55c2f33143cbfb4388046dced913e7956530
Glenn <skeltong2@gmail.com>
2018-08-08 22:02:39 -0600

    Initial Commit

---
+++ Work JournalTests/Work_JournalTests.swift
@@ -0,0 +1,36 @@
+//
+//  Work_JournalTests.swift
+//  Work JournalTests
+//
+//  Created by Glenn Skelton on 2018-08-08.
+//  Copyright © 2018 Glenn Skelton. All rights reserved.
+//
+
+import XCTest
+@testable import Work_Journal
+
+class Work_JournalTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
